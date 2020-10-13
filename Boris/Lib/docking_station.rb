558928b37659ffa0ecde69f2fe6a605422667574
Esther <esthercog97@gmail.com>
2020-10-13 16:42:43 +0100

    Dock Bike and See Bike (with rspec)

--- Boris/Lib/docking_station.rb
+++ Boris/Lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :docked_bike
+
   def release_bike
     return Bike.new
+  end
+
+  def dock_bike(bike)
+    @docked_bike = bike
+  end
+
+  def see_bike
+    self.docked_bike
   end
 end
--- Boris/Spec/docking_station_spec.rb
+++ Boris/Spec/docking_station_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe DockingStation do
       expect(subject.release_bike.working?).to eq true
     end
   end
+
+  describe "#dock_bike" do
+    it "Docks a bike" do
+      expect(subject).to respond_to(:dock_bike).with(1).argument
+    end
+  end
+
+  describe "#see_bike" do
+    it "Shows a docked bike" do
+      bike = DockingStation.new.release_bike
+      subject.dock_bike(bike)
+      expect(subject.see_bike).to eq bike
+    end
+  end
+
 end
